70702485ae66299dfe55de1c3a39d934a74ffd03
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2020-07-31 16:35:50 -0400

    New exception added

--- upper_case_exception.rb
+++ upper_case_exception.rb
@@ -5,7 +5,8 @@ class UpperCaseException < StandardError
 end
 
 def divideBy0
-  mem = GetProcessMem.new
+  #mem = GetProcessMem.new
+  #puts mem.inspect
   print "Ingrese el un numero: "
   num1 = gets.to_i
   print "\nIngrese otro numero: "
@@ -19,8 +20,25 @@ def divideBy0
     elapsed = clock_then - clock_now
   end
   puts "Tiempo de la excepcion #{elapsed} s"
-  puts "Memoria total #{mem.kb} kb"
+
+  #puts "Memoria total #{mem.kb} kb"
+end
+
+def myException
+  elapsed = nil
+  print "Dijite una frase con mayusculas: "
+  phrase = gets.chomp
+
+  begin
+    clock_now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    raise UpperCaseException if phrase =~ /[A-Z]/
+  rescue
+    clock_then = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+  elapsed = clock_then - clock_now
+  puts "Tiempo tomado para la excepcion: #{elapsed} s"
+
 end
 
 divideBy0
-#raise UpperCaseException
+myException
